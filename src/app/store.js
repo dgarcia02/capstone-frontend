fa17aaa7be981b173c73f1f10036cbdb30abd10f
Dulce Garcia <dulcegarcia@Dulces-MacBook-Air.local>
2021-09-06 11:28:51 -0500

    modal is setup correctly

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import isLoggedReducer from '../features/isLogged/isLoggedSlice';
+import currentUserSlice from '../features/currentUser/currentUserSlice';
 import profileReducer from '../features/profiles/profileSlice';
 import newUserReducer from '../features/userAuth/newUserSlice';
 import { showsApiSlice } from '../api/shows/shows-api-slice';
@@ -10,7 +10,7 @@ export const store = configureStore({
     // key names will define the state
     reducer: {
         counter: counterReducer,
-        isLogged: isLoggedReducer,
+        currentUser: currentUserSlice,
         profile: profileReducer,
         newUser: newUserReducer,
         [showsApiSlice.reducerPath]: showsApiSlice.reducer,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logIn  } from '../features/currentUser/currentUserSlice'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 // Components
-// import LoginForm from './LogIn';
+import currentUser from '../features/currentUser/currentUserSlice'
 
 const Navbar = () => {
-    const isLogged = useSelector(state => state.isLogged)
+    const dispatch = useDispatch()
+    // come back and check if this works correctly
+    const currentUser = useSelector(state => state.currentUser)
+
+    const [show, setShow] = useState(false)
+
+    // this opens and closes the modal
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
     return (
         <div className="nav-container">
-            <h3>this is the navbar test</h3>
-            {/* <nav className="navbar">
+            <nav className="navbar">
+                <Button variant='warning' onClick={handleShow}>
+                    Launch demo modal
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>  
+
                 <h2 id="nav-logo">Logo</h2>
                 <div className="menu-toggle" id="mobile-menu">
                     <span className="bar"></span>
@@ -23,20 +50,21 @@ const Navbar = () => {
                         <a href="#" className="nav-links">Home</a>
                     </li>
                     {
-                        isLogged ?
+                        currentUser ?
                         <li><a href="#" className="nav-links">Profile</a></li>
                         : ''
                     }
 
                     {
-                        isLogged ?
+                        currentUser ?
                         <li>
                             <button onClick={()=> dispatch(logIn())}>Log Out</button>
                         </li>
                         :
                         <li>
-                            <button onClick={()=> dispatch(open_close())}>Log In</button>
-                            <modal
+                            <button onClick={()=> dispatch(logIn())}>Log In</button>
+                            {/* <button onClick={()=> dispatch(open_close())}>Log In</button> */}
+                            {/* <modal
                                 open={ loginOpen }
                                 onClose={ ()=>dispatch(open_close()) }
                                 center
@@ -44,13 +72,13 @@ const Navbar = () => {
                                     overlay: 'customOverlay',
                                     modal: 'customModal',
                                 }} >
-                                {/* <LoginForm /> 
-                            </modal>
+                                <LoginForm /> 
+                            </modal> */}
                         </li>
                     } 
                    
                 </ul>
-            </nav> */}
+            </nav>
         </div>
     )
 }
