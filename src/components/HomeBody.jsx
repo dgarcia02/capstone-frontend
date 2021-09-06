907c35c220c8d8a151ecdd7e48d092cf291d3438
Dulce Garcia <dulcegarcia@Dulces-Air.lan1>
2021-09-06 15:43:15 -0500

    created the modal for the shows info

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 // API
 import { useFetchShowsQuery } from '../api/services/shows-api-slice';
@@ -11,6 +12,12 @@ import { useFetchShowsQuery } from '../api/services/shows-api-slice';
 
 const HomeBody = () => {
     const { data, error, isLoading } = useFetchShowsQuery();
+
+    const [show, setShow] = useState(false)
+
+    // this opens and closes the modal
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true) 
 
     return (
         <>
@@ -26,19 +33,32 @@ const HomeBody = () => {
                             <div className='allShows'>
                                 {data._embedded.events.map((event) => (
                                     <div className='individualShow'>
-                                        <img src={event.images[0].url} alt={event.name} height={250} />
+                                        <img src={event.images[0].url} alt={event.name} height={250} onClick={handleShow} />
+                                        <Modal show={show} onHide={handleClose}>
+                                            <Modal.Header closeButton>
+                                                <Modal.Title>{event.name}</Modal.Title>
+                                            </Modal.Header>
+                                            <Modal.Body>
+                                                <img src={event.images[0].url} alt={event.name} height={250} />
+                                                <h6>{event.classifications[0].genre.name}</h6>
+                                                <h6>{event._embedded.venues[0].name}</h6>
+                                                <h6>{event.dates.start.localDate}</h6>
+                                                <h6>{event.dates.start.localTime}</h6>
+                                                <h6>${event.priceRanges[0].min}</h6>
+                                                <p>Please Note: {event.info}</p>
+                                                <Button variant="outline-secondary" type="submit">
+                                                    <i class="fas fa-heart"></i>
+                                                </Button>
+                                                <Button variant="outline-secondary" type="submit">
+                                                    <a href={event.url} target='_blank'>Buy Tickets</a>
+                                                </Button>
+                                            </Modal.Body>
+                                            <Modal.Footer>
+                                            </Modal.Footer>
+                                        </Modal>
                                         <h3>{event.name}</h3>
-                                        <h6>{event.classifications[0].genre.name}</h6>
-                                        <h6>{event._embedded.venues[0].name}</h6>
-                                        <h6>{event.dates.start.localDate}</h6>
-                                        <h6>{event.dates.start.localTime}</h6>
-                                        <h6>${event.priceRanges[0].min}</h6>
-                                        <p>Please Note: {event.info}</p>
                                         <Button variant="outline-secondary" type="submit">
                                             <i class="fas fa-heart"></i>
-                                        </Button>
-                                        <Button variant="outline-secondary" type="submit">
-                                            <a href={event.url} target='_blank'>Buy Tickets</a>
                                         </Button>
                                     </div>
                                  ))}
